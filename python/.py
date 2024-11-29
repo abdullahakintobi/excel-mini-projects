a9e95a1cbd3e04d013d57427900ce3f617af9504
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-29 22:26:16 +0100

    write a for loop statement
    For loop statement for even and odd numbers using the range function

--- python/.py
+++ python/.py
@@ -1,1 +1,8 @@
-print(python)
+print("python")
+
+for x in range(1, 100, 2):
+        if x%2 = 0:
+                print("This is an even number")
+
+        if x%2 != 0:
+                print("This is an odd number")
